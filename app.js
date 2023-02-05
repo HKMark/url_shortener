09d5fc0bcddb478982925fa5a38e118a02aa87e9
HKMark <hongkongmark@gmail.com>
2023-02-05 11:24:02 +0800

    feat: add short links generator

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Urls = require('./models/shortener')
 
 // require dotenv if NODE_ENV is not production
 if (process.env.NODE_ENV !== 'production') {
@@ -25,8 +27,38 @@ db.once('open', () => {
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/shorten', (req, res) => {
+  const original_links = req.body.name
+
+  // pop-up alert if no URL inputted
+
+  // Check whether the original link is already in the database
+
+  // generate the short links
+  const generateRandomString = (num) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let result = ''
+    const charactersLength = characters.length
+    for (let i = 0; i < num; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
+  }
+  const RandomString = generateRandomString(5)
+  const host = req.get('origin')
+  const short_links = host + "/" + RandomString
+  return Urls.create({ 
+    original_links: original_links,
+    short_links: short_links
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
